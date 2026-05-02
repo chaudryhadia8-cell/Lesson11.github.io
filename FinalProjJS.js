c3a54189988b1d61a501b051dbf7befb560ebfe3
chaudryhadia8-cell <chaudryhadia8@gmail.com>
2026-05-02 14:02:17 -0400

    Delete FinalProjJS.js

--- FinalProjJS.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('buttonChange');
-const textChange = document.getElementById('changeMessage');
-
-btn.onclick = function() {
-    textChange.textContent = "The text has been changed!";
-};
